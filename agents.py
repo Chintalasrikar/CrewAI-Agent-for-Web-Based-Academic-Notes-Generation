86bddfcb81f105568196ce1a29602e8e456061d4
Srikar Chintala <134192460+Chintalasrikar@users.noreply.github.com>
2025-07-19 18:50:39 +0530

    Update agents.py

--- agents.py
+++ agents.py
@@ -8,7 +8,7 @@ from langchain_groq import ChatGroq
 load_dotenv()
 GROQ_API_KEY = os.getenv("GROQ_API_KEY")
 llm = ChatGroq(
- model="llama-3.3-70b-versatile",
+ model="meta-llama/llama-4-scout-17b-16e-instruct",
  temperature=0.7,
  api_key=GROQ_API_KEY
 )
